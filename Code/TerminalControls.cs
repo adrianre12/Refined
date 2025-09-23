08b05d5a12993c01f97e509891663eadfabcec0f
adrianre12 <adrianrevill@hotmail.com>
2025-09-23 15:35:44 +0100

    Hide unwanted controls

--- Code/TerminalControls.cs
+++ Code/TerminalControls.cs
@@ -1,5 +1,8 @@
 ﻿using Sandbox.ModAPI;
+using Sandbox.ModAPI.Interfaces;
 using Sandbox.ModAPI.Interfaces.Terminal;
+using System.Collections.Generic;
+using VRage;
 using VRage.Game.ModAPI;
 using VRage.Utils;
 
@@ -16,12 +19,18 @@ namespace Catopia.Refined
                 return;
             Done = true;
 
+            EditControls();
             CreateControls();
         }
 
         static bool CustomVisibleCondition(IMyTerminalBlock b)
         {
             return b?.GameLogic?.GetAs<RefinedBlock>() != null;
+        }
+
+        static bool CustomHiddenCondition(IMyTerminalBlock b)
+        {
+            return b?.GameLogic?.GetAs<RefinedBlock>() == null;
         }
 
         static void CreateControls()
@@ -44,6 +53,42 @@ namespace Catopia.Refined
                 c.Action = (b) => { b?.GameLogic?.GetAs<RefinedBlock>()?.TestButtonToggle(); };
 
                 MyAPIGateway.TerminalControls.AddControl<IMyTextPanel>(c);
+            }
+        }
+
+
+        static void EditControls()
+        {
+            List<IMyTerminalControl> controls;
+
+            MyAPIGateway.TerminalControls.GetControls<IMyTextPanel>(out controls);
+
+            foreach (IMyTerminalControl c in controls)
+            {
+                // a quick way to dump all IDs to SE's log
+                string name = MyTexts.GetString((c as IMyTerminalControlTitleTooltip)?.Title.String ?? "N/A");
+                string valueType = (c as ITerminalProperty)?.TypeName ?? "N/A";
+                Log.Msg($"[DEV] terminal property: id='{c.Id}'; type='{c.GetType().Name}'; valueType='{valueType}'; displayName='{name}'");
+
+                switch (c.Id)
+                {
+                    case "OnOff":
+                    case "ShowInTerminal":
+                    case "ShowInInventory":
+                    case "ShowInToolbarConfig":
+                    case "Name":
+                    case "ShowOnHUD":
+                    case "CustomData":
+                        {
+                            break;
+                        }
+                    default:
+                        {
+                            //c.Enabled = TerminalChainedDelegate.Create(c.Enabled, CustomHiddenCondition); // grays out
+                            c.Visible = TerminalChainedDelegate.Create(c.Visible, CustomHiddenCondition); // hides
+                            break;
+                        }
+                }
             }
         }
     }
